f48309525d5af4d28443e93a742fd1c90ce25409
Emil Sharier <emil.sharier.dev@gmail.com>
2026-07-14 17:12:22 +0530

    Initial Commit

---
+++ scratio/scratioApp.swift
@@ -0,0 +1,17 @@
+//
+//  scratioApp.swift
+//  scratio
+//
+//  Created by Emil Sharier on 14/07/26.
+//
+
+import SwiftUI
+
+@main
+struct scratioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ scratioTests/scratioTests.swift
@@ -0,0 +1,17 @@
+//
+//  scratioTests.swift
+//  scratioTests
+//
+//  Created by Emil Sharier on 14/07/26.
+//
+
+import Testing
+@testable import scratio
+
+struct scratioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
